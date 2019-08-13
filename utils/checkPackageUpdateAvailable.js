02fc50aa7aa994286cfdcf0da757395756316e23
Vitalii Obideiko <vitaliy.obideyko@gmail.com>
2019-08-13 16:01:31 +0300

    Change notification for update package

--- utils/checkPackageUpdateAvailable.js
+++ utils/checkPackageUpdateAvailable.js
@@ -4,10 +4,17 @@ const updateNotifier = require("update-notifier");
 // Checks for available update and returns an instance
 const notifier = updateNotifier({
   pkg,
-  shouldNotifyInNpmScript: true
+  shouldNotifyInNpmScript: true,
+  updateCheckInterval: 0
 });
 
-// Notify using the built-in convenience method
-notifier.notify({
-  isGlobal: true
-});
+if (notifier.update && notifier.update.latest !== pkg.version) {
+  const old = notifier.update.current;
+  const latest = notifier.update.latest;
+  const type = notifier.update.type;
+  notifier.notify({
+    message:
+      `New ${type} version of ${pkg.name} available! ${old} -> ${latest}\n` +
+      `Run ${`npm i -g ${pkg.name}`} to update!`
+  });
+}
